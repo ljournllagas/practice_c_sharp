2f46ec9786945172639d0f4bb8a2f70b9af7f16a
ljournllagas <40813696+ljournllagas@users.noreply.github.com>
2019-11-02 13:18:11 +0800

    Add project files.

---
+++ PracticeCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
